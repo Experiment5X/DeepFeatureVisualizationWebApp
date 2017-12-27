2b9f3f58b43362fba5278d1f220f74fdd6efe3a6
Adam Spindler <xmeadamx@gmail.com>
2017-12-27 14:38:50 -0500

    Match initial layer match the diagram

--- DeepFeaturesApp/forms.py
+++ DeepFeaturesApp/forms.py
@@ -3,7 +3,7 @@ from django import forms
 
 class DeepFeatureForm(forms.Form):
     learning_rate = forms.FloatField(label='Learning Rate', initial=0.1)
-    layer_index = forms.IntegerField(label='Layer Index', initial=-3)
+    layer_index = forms.IntegerField(label='Layer Index', initial=2)
     image_std_clip = forms.FloatField(label='Image Std Clip', initial=3)
     grad_std_clip = forms.FloatField(label='Gradient Std Clip', initial=1.5)
     epoch_count = forms.IntegerField(label='Number of Epochs', initial=250)
